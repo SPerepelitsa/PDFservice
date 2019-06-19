010b4901417d9cf61e321a6714493d5353efaf23
SPerepelitsa <sergei.poltava@gmail.com>
2019-06-19 23:28:48 +0300

    pdf_files table added

---
+++ database/migrations/2019_06_19_195901_create_pdf_files_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePdfFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pdf_files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->mediumText('key_words');
+            $table->json('metainfo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pdf_files');
+    }
+}
